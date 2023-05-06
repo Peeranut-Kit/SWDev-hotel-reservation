ff06897eade265bf52a251eebb700dbdecd55be0
Peeranut "Max" Kit <peeranut.kit.work@gmail.com>
2023-05-06 10:59:38 +0700

    Update User schema

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,11 +6,11 @@ const User = require('../models/User');
 
 exports.register = async (req,res,next)=> {
     try {
-        const {name, email, password, role} = req.body;
+        const {name, tel, email, password, role} = req.body;
 
         //Create user
         const user = await User.create({
-            name, email, password, role
+            name, tel, email, password, role
         });
         //Create token to cookie and send response
         sendTokenResponse(user,200,res);
@@ -72,6 +72,7 @@ const sendTokenResponse = (user, statusCode, res) => {
         _id: user._id,
         name: user.name,
         email: user.email,
+        tel: user.tel,
         //end for frontend
         token
     });
